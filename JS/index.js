2ae6bf83726ad68a5c845b760e72d762866e71e6
Adrian.M <113439215+AdrianM018@users.noreply.github.com>
2024-07-15 14:39:00 +0300

    js search functionality

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,28 @@
-BookSearchFetch()
+subjectlisting()
 
-async function BookSearchFetch() {
+async function subjectlisting(){
+    const urlsubjects = `https://openlibrary.org/subjects/crime.json`
+try{
 
-    const booksearch_input = document.getElementsByClassName("booksearch_input").value;
+    const response = await fetch(urlsubjects);
+    const responseindata = await response.json();
+    const onebookpersubject = await responseindata.edition_count
+    console.log(responseindata.works)
 
-    
+} catch(error){
+    console.log(error)
+}
+
+}
+
+async function searchingbooks(){
+
+    try{
+        const booksearch_input = document.getElementById("booksearch_input").value.toLocaleLowerCase();
+        const searchURL = await fetch(`https://openlibrary.org/search.json?title=${booksearch_input}`)
+        const datasearch = await searchURL.json()
+        console.log(datasearch.docs[0].title)
+    }catch(error){
+        console.log(error)
+    }
 }
